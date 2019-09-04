3d94423eb6dfafc7b8b5dfef65daf64947d4cf2c
Cristopher Namchee <cristophernamchee12@gmail.com>
2019-09-04 20:38:21 +0700

    Working on app drawer

--- app/src/main/java/com/example/calculatingwombat/MainActivity.java
+++ app/src/main/java/com/example/calculatingwombat/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.calculatingwombat;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-
+import android.view.Window;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+
+        window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+
+        Toolbar toolbar = this.findViewById(R.id.toolbar_main);
+
+        this.setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = this.findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle =  new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+
+        drawerLayout.addDrawerListener(toggle);
     }
 }
